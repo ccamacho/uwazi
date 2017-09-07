5fc1f1eedaad84f79f140d46f5be658f90a1dc8a
A Happy Cat <acasadotorres@gmail.com>
2017-09-07 15:09:13 +0200

    mend

--- app/api/search/specs/performance.spec.js
+++ app/api/search/specs/performance.spec.js
@@ -1,35 +1,37 @@
 import elasticConfig from 'api/config/elasticIndexes.js';
 elasticConfig.index = elasticConfig.development;
+import mongoose from 'mongoose';
+mongoose.connect('mongodb://localhost/uwazi_development', {useMongoClient: true});
 import search from '../search.js';
 
 const queries = [
   {searchTerm: 'echos de la'},
-  {order: 'desc', searchTerm: 'victima', sort: 'metadata.fecha'},
   {searchTerm: 'corte'},
+  {order: 'desc', searchTerm: 'victima', sort: 'metadata.fecha'},
+  {order: 'asc', searchTerm: 'victima', sort: 'metadata.fecha'},
+  {filters: {pa_s: {values: ['q223u6ha4zsq0k9']}}, order: 'desc', searchTerm: 'victima', sort: 'metadata.fecha', types: ['58b2f3a35d59f31e1345b4ac','58b2f3a35d59f31e1345b471','58b2f3a35d59f31e1345b482','58b2f3a35d59f31e1345b479']},
   {searchTerm: 'resolucion interamericana'},
-  {order: 'asc', searchTerm: 'victima', sort: 'metadata.fecha'}
+  {order: 'desc', searchTerm: 'resolucion interamericana', sort: 'metadata.fecha'},
+  {filters: {pa_s: {values: ['q223u6ha4zsq0k9']}}, order: 'desc', searchTerm: 'resolucion interamericana', sort: 'metadata.fecha', types: ['58b2f3a35d59f31e1345b4ac','58b2f3a35d59f31e1345b471','58b2f3a35d59f31e1345b482','58b2f3a35d59f31e1345b479']},
+  {searchTerm: 'guatemala'},
+  {order: 'desc', searchTerm: 'guatemala', sort: 'metadata.fecha'},
+  {filters: {pa_s: {values: ['q223u6ha4zsq0k9']}}, order: 'desc', searchTerm: 'guatemala', sort: 'metadata.fecha', types: ['58b2f3a35d59f31e1345b4ac','58b2f3a35d59f31e1345b471','58b2f3a35d59f31e1345b482','58b2f3a35d59f31e1345b479']}
 ];
 
-fdescribe('elasticsearch performance', () => {
-  it('should not exceed an average of 200ms', (done) => {
-    Promise.all(queries.map((query, index) => {
-      return new Promise((resolve) => {
-        setTimeout(() => {
-          search.search(query, 'es')
-          .then((r) => resolve(r));
-        }, index * 1000);
-      });
-    }))
-    .then((results) => {
-      return results.reduce((t, r) => {
-        console.log('took:', r.took);
-        return t + r.took;
-      }, 0) / results.length;
-    })
-    .then((total) => {
-      console.log(total);
-      expect(total < 200).toBe(true);
-      done();
-    });
-  }, 10000);
+Promise.all(queries.map((query, index) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      search.search(query, 'es')
+      .then((r) => resolve(r));
+    }, index * 1000);
+  });
+}))
+.then((results) => {
+  return results.reduce((t, r) => {
+    console.log('took:', r.took, 'total hits:', r.totalRows);
+    return t + r.took;
+  }, 0) / results.length;
+})
+.then((total) => {
+  console.log(total);
 });
---
+++ query_performance_test.js
@@ -0,0 +1,2 @@
+require('babel-core/register')();
+require('./app/api/search/specs/performance.spec.js');
